58891b237df2fe78f443be7964a0c3c46ac0e2e8
김명수 <mskim@jstair.com>
2024-03-07 16:25:01 +0900

    다익스트라 길찾기 알고리즘 구현

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -4,12 +4,12 @@
     {
         int[,] adj = new int[6,6]
         {
-            { 0, 1, 0, 1, 0, 0 },
-            { 1, 0, 1, 1, 0, 0 },
-            { 0, 1, 0, 0, 0, 0 },
-            { 1, 1, 0, 0, 1, 0 },
-            { 0, 0, 0, 1, 0, 0 },
-            { 0, 0, 0, 0, 1, 0 },
+            { -1, 15, -1, 35, -1, -1 },
+            { 15, -1, 05, 10, -1, -1 },
+            { -1, 05, -1, -1, -1, -1 },
+            { 35, 10, -1, -1, 05, -1 },
+            { -1, -1, -1, 05, -1, 05 },
+            { -1, -1, -1, -1, 05, -1 },
         };
 
         List<int>[] adj2 = new List<int>[]
@@ -21,6 +21,72 @@
             new List<int>() { 3, 5 },
             new List<int>() { 4 }
         };
+
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+
+            // 방문을 못해서 0인지 초기 값이 0인지 헷갈리니까 Int32의 최댓값을 채워줌
+            Array.Fill(distance, Int32.MaxValue);
+
+            distance[start] = 0;
+            parent[start] = start;
+
+            while (true)
+            {
+                // 제일 가까이에 있는 후보를 찾는다. 
+
+
+                // 가장 유력한 후보의 거리와 번호를 저장
+                int closest = Int32.MaxValue;
+                int now = -1;
+
+                for (int i = 0; i < 6; i++)
+                {
+                    // 이미 방문한 점은 스킵
+                    if (visited[i])
+                        continue;
+                    // 아직 발견된 적이 없거나, 기존 후보보다 멀리 있으면 스킵
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
+                    // 지금까지 발견한 가장 적합한 후보이므로 정보를 갱신
+                    closest = distance[i];
+                    now = i;
+                }
+
+                // 적합한 후보가 하나도 없을 때 -> 종료
+                if (now == -1)
+                    break;
+
+                // 제일 좋은 후보를 찾았으므로 이동한다. 
+                visited[now] = true;
+
+                // 방문한 정점과 인접한 점들을 조사해서 상황에 따라 발견한 최단거리를 갱신한다. 
+                for (int next = 0; next < 6; next++)
+                {
+                    // 연결되지 않은 점은 스킵 
+                    if (adj[now, next] == -1)
+                        continue;
+                    // 이미 방문했던 점도 스킵
+                    if (!visited[next])
+                        continue;
+
+                    // 새로 조사된 점의 최단 거리를 계산한다. 
+                    int nextDist = distance[now] + adj[now, next];
+
+                    // 만약 기존에 발견한 최단 거리가 새로 조사한 거리보다 크면 정보를 갱신
+                    if (nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
+                } 
+
+            }
+            
+        }
 
         public void BFS(int start)
         {
