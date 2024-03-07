8bc883a72e7a2bb95f0f6a787de70fbb7bab83cc
김명수 <mskim@jstair.com>
2024-03-07 14:42:55 +0900

    BFS구현

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -7,8 +7,8 @@
             { 0, 1, 0, 1, 0, 0 },
             { 1, 0, 1, 1, 0, 0 },
             { 0, 1, 0, 0, 0, 0 },
-            { 1, 1, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0 },
+            { 1, 1, 0, 0, 1, 0 },
+            { 0, 0, 0, 1, 0, 0 },
             { 0, 0, 0, 0, 1, 0 },
         };
 
@@ -17,10 +17,38 @@
             new List<int>() { 1, 3 },
             new List<int>() { 0, 2, 3 },
             new List<int>() { 1 },
-            new List<int>() { 0, 1 },
-            new List<int>() { 5 },
+            new List<int>() { 0, 1, 4 },
+            new List<int>() { 3, 5 },
             new List<int>() { 4 }
         };
+
+        public void BFS(int start)
+        {
+            bool[] found = new bool[6];
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+
+            while (q.Count > 0)
+            {
+                int now = q.Dequeue();
+
+                for (int next = 0; next < 6; next++)
+                {
+                    // 인접하지 않았으면 스킵
+                    if (adj[now, next] == 0)
+                        continue;
+                    // 이미 발견했어도 스킵
+                    if (found[next])
+                        continue;
+
+                    q.Enqueue(next);
+                    found[next] = true;
+                }
+            }
+        }
+
 
         bool[] visited = new bool[6];
         public void DFS(int now)
@@ -65,7 +93,6 @@
                     DFS(now);
             }
         }
-
     }
 
     class Program
@@ -75,7 +102,7 @@
             Graph graph = new Graph();
             //graph.DFS(0);
             //graph.DFS2(0);
-            graph.SearchAll();
+            //graph.SearchAll();
         }
     }
 }
